df08736a00d0af0b766c26680382191810a5dcc5
Omp <kantee2540@hotmail.com>
2020-04-22 16:48:47 +0700

    Add pull to refresh

--- app/src/main/java/com/example/bitcoin/MainActivity.kt
+++ app/src/main/java/com/example/bitcoin/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,11 +14,17 @@ class MainActivity : AppCompatActivity(), DownloadBitcoinInterface {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        downloadBitcoinItem()
+
+        pull_to_refresh.setOnRefreshListener {
+            downloadBitcoinItem()
+        }
+
+    }
+
+    private fun downloadBitcoinItem() {
         val downloadBitcoin = DownloadBitcoinItem(this)
         downloadBitcoin.downloadItem()
-
-
-
     }
 
     override fun downloadItemSuccess(item: ArrayList<BitcoinModel>) {
@@ -26,10 +33,12 @@ class MainActivity : AppCompatActivity(), DownloadBitcoinInterface {
             bitcoin_recycler.adapter = bitcoinRecyclerAdapter
             bitcoin_recycler.layoutManager = LinearLayoutManager(this)
         }
+        pull_to_refresh.isRefreshing = false
 
     }
 
     override fun downloadItemFailed(error: String) {
-
+        Toast.makeText(this, R.string.error_mes, Toast.LENGTH_LONG).show()
+        pull_to_refresh.isRefreshing = false
     }
 }
