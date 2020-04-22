9ccafc936241fbb8fd6fc4a5379b40e12db4a00d
Omp <kantee2540@hotmail.com>
2020-04-22 18:21:10 +0700

    Can download image

--- app/src/main/java/com/example/bitcoin/BitcoinRecyclerAdapter.kt
+++ app/src/main/java/com/example/bitcoin/BitcoinRecyclerAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.bitcoin
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
 import kotlinx.android.synthetic.main.bitcoin_recycler_item.view.*
 import kotlinx.android.synthetic.main.bitcoin_recycler_item.view.recycler_icon
 import kotlinx.android.synthetic.main.bitcoin_recycler_item.view.recycler_name
+import kotlinx.android.synthetic.main.bitcoin_recycler_item2.view.*
 
 class BitcoinRecyclerAdapter(private val bitcoinItem: ArrayList<BitcoinModel>, private val context: Context)
     : RecyclerView.Adapter<BitcoinRecyclerAdapter.Holder>(){
@@ -18,12 +21,17 @@ class BitcoinRecyclerAdapter(private val bitcoinItem: ArrayList<BitcoinModel>, p
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val bitCoinItem = bitcoinItem[position]
+        val iconURI = Uri.parse(bitCoinItem.iconURL)
 
         if(position % 5 == 4 && position != 0) {
-            holder.recyclerName.text = bitCoinItem.name
+            holder.recyclerName2.text = bitCoinItem.name
+            GlideToVectorYou.init().with(context).load(iconURI, holder.recyclerIcon2)
+
         }else{
             holder.recyclerName.text = bitCoinItem.name
             holder.recyclerDescription.text = bitCoinItem.description
+            GlideToVectorYou.init().with(context).load(iconURI, holder.recyclerIcon)
+
         }
     }
 
@@ -46,8 +54,13 @@ class BitcoinRecyclerAdapter(private val bitcoinItem: ArrayList<BitcoinModel>, p
     }
 
     class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //For standard layout
         val recyclerName = itemView.recycler_name
         val recyclerDescription = itemView.recycler_description
         val recyclerIcon = itemView.recycler_icon
+
+        //For right layout
+        val recyclerName2 = itemView.recycler_name2
+        val recyclerIcon2 = itemView.recycler_icon2
     }
 }
